8ad70e1ddc8d4c7d518d36f604034ef5ee8f4513
Diogo L Scarmagnani <diogols@live.com>
2024-03-02 18:58:41 -0300

    App.tsx

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # stylecut
 
 This is my web app called *StyleCut*. Enjoy it.
+
+npm i react-router-dom
+npm install axios
+npm install localforage
+npm install react-hook-form
+npm install zod
+npm i @hookform/resolvers
+
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+
---
+++ src/App.tsx
@@ -0,0 +1,128 @@
+import './index.css'
+// import Container from 'react-bootstrap/Container'
+import { Outlet, Route, RouterProvider, createBrowserRouter, createRoutesFromElements, useLoaderData, useParams } from 'react-router-dom'
+// import Home from './Home'
+// import Header from './components/Header'
+// import Footer from './components/Footer'
+// import Names from './components/Names/Names'
+// import NameDetails from './components/Names/NameDetails'
+// import FormName from './components/Names/FormName'
+// import Phones from './components/Phones/Phones'
+// import PhoneDetails from './components/Phones/PhoneDetails'
+// import FormPhone from './components/Phones/FormPhone'
+// import About from './About'
+import axios from 'axios'
+import localforage from 'localforage'
+// import DefaultLayout from './DefaultLayout'
+import { redirect } from 'react-router-dom/dist'
+
+const NotFound = () => <div>Not foud</div>
+
+// const namesToCreateObj = ['Diogo', 'Karla', 'Lucas', 'Pablo', 'Fabrício']
+// const names = namesToCreateObj.map(name => ({ name, nameId: crypto.randomUUID() }))
+
+// const NamesLayout = () => {
+//   return (
+//     <>
+//       <Header />
+
+//       <Container fluid>
+//         <h1>Names</h1>
+//         <Outlet />
+//       </Container>
+
+//       <Footer />
+//     </>
+//   )
+// }
+
+// const defaultPhonesLoader = async ({ seed }) => {
+//   const hasSeed = await localforage.getItem('seed')
+//   const hasPeople = await localforage.getItem('people')
+
+//   if (hasPeople && (!seed || hasSeed === seed)) {
+//     return hasPeople
+//   }
+
+//   const inc = ['gender', 'name', 'email', 'cell', 'login', 'picture'].join(',')
+//   const nat = 'br'
+//   const results = 10
+
+//   const urlParams = new URLSearchParams({ results, nat, inc }).toString()
+//   const url = `https://randomuser.me/api/?${urlParams}`
+//   const { data } = await axios.get(url)
+
+//   const people = data.results.map((item) => ({ ...item, seed: data.info.seed }))
+
+//   await localforage.setItem('seed', data.info.seed)
+//   await localforage.setItem('people', people)
+
+//   return people
+// }
+
+// const phonesLoader = async ({ params: { seed, uuid } }) => {
+//   const people = await defaultPhonesLoader({ seed, uuid })
+
+//   if (uuid) {
+//     return people?.find(p => p.login.uuid === uuid) ?? null
+//   }
+
+//   return people
+// }
+
+// const PhonesLayout = () => {
+//   return (
+//     <>
+//       <Header />
+
+//       <Container fluid>
+//         <h1>Phones</h1>
+//         <Outlet />
+//       </Container>
+
+//       <Footer />
+//     </>
+//   )
+// }
+
+function AaaiNovinhaSentaAPampa() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // <Route path="/" element={<DefaultLayout />} >
+    <Route path="/" element={<AaaiNovinhaSentaAPampa />} >
+      {/* <Route index element={<Home title="Home" />} /> */}
+
+      {/* <Route path="about" element={<About />} /> */}
+
+      {/* <Route path="names" element={<NamesLayout />}> */}
+      {/*   <Route index element={<Names names={names} />} /> */}
+      {/*   <Route path=":id" element={<NameDetails names={names} />} /> */}
+      {/*   <Route path=":id/edit" element={<NameDetails names={names} />} /> */}
+      {/*   <Route path=":id/delete" element={<NameDetails names={names} />} /> */}
+      {/*   <Route path="new" element={<FormName names={names} />} /> */}
+      {/* </Route> */}
+
+      {/* <Route path="phones"> */}
+      {/*   <Route index element={<Phones title="Todos os telefones" />} loader={phonesLoader} /> */}
+      {/*   <Route path=":seed" element={<Phones title="Todos os telefones" />} loader={phonesLoader} /> */}
+      {/*   <Route path=":seed/:uuid" element={<PhoneDetails title="Telefone específico" />} loader={phonesLoader} /> */}
+      {/*   <Route path=":id/edit" element={<PhoneDetails />} /> */}
+      {/*   <Route path=":id/delete" element={<PhoneDetails />} /> */}
+      {/*   <Route path="new" element={<FormPhone title="Novo telefone" />} /> */}
+      {/* </Route> */}
+
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+)
+
+const App = () => <RouterProvider router={router} />
+
+export default App
